253ae23b89261095b40735796cc8c81461e13ab8
mostovsky <mostovsky@ya.ru>
2020-04-19 22:08:14 +0300

    some ok

--- CMD_extensions/all_cmd_ext.reg_run.cmd
+++ CMD_extensions/all_cmd_ext.reg_run.cmd
@@ -1,7 +1,15 @@
-REM @echo off
+@echo off
 echo INSTALL CMD_EXTension by adding in reg
 REM reg import "%~dp0\all_cmd_ext.reg"
+:: Default variant
 reg import "%~dp0all_cmd_ext.reg"
+:: on the 64-bit variant
+:: http://forum.oszone.net/thread-193042.html
+:: HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft - to exclude "WOW6432Node"
+REM cmd /c "%windir%\system32\cmd.exe" /c "reg import %~dp0all_cmd_ext.reg"
+reg import "%~dp0all_cmd_ext.reg" /reg:32
+reg import "%~dp0all_cmd_ext.reg" /reg:64
+
 if '%errorlevel%' neq '0' (
 	echo Adding FAIL
 	color 40 & title=Adding FAIL
